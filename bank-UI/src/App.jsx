39216e7477d44589dc02fe3e13338d9c293daf90
yash57-ty <thummaryash969@gmail.com>
2026-01-22 23:38:35 +0530

    backend strated

---
+++ bank-UI/src/App.jsx
@@ -0,0 +1,9 @@
+import BankLinking from "./Component/BankLinking";
+
+export default function App() {
+  return (
+    <div className="h-full w-full bg-neutral-900 flex items-center justify-center">
+      <BankLinking />
+    </div>
+  );
+}
