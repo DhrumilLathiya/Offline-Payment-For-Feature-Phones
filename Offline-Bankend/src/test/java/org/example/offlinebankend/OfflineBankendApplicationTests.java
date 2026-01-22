be9c8cb6ce7a80a9751d58eddd5e4f7ca2aecfed
yash57-ty <thummaryash969@gmail.com>
2026-01-22 23:31:19 +0530

    backend strated

---
+++ Offline-Bankend/src/test/java/org/example/offlinebankend/OfflineBankendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.offlinebankend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineBankendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
