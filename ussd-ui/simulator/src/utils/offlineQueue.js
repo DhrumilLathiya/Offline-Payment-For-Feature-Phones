c48aa3918d556a8ad5d8d6cfebe9dafd82c59419
yash_718 <thummaryash969@gmail.com>
2026-01-24 22:35:22 +0530

    Delete ussd-ui directory
    delete

--- ussd-ui/simulator/src/utils/offlineQueue.js
+++
@@ -1,5 +0,0 @@
-export function saveOffline(txn) {
-  let q = JSON.parse(localStorage.getItem("offlineQueue")) || [];
-  q.push(txn);
-  localStorage.setItem("offlineQueue", JSON.stringify(q));
-}
--- ussd-ui/simulator/src/utils/ussdsession.js
+++
@@ -1,8 +0,0 @@
-let timer = null;
-
-export function startSession(onExpire) {
-  clearTimeout(timer);
-  timer = setTimeout(() => {
-    onExpire();
-  }, 60000); // 60 sec timeout
-}
